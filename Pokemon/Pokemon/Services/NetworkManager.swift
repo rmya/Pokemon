84d064df3da1a89961de1afeadcc52c5228042ff
Rumeysa Tan <tanrumeysa93@gmail.com>
2022-08-10 18:41:00 +0300

    network functions added

--- Pokemon/Pokemon/Services/NetworkManager.swift
+++ Pokemon/Pokemon/Services/NetworkManager.swift
@@ -11,4 +11,36 @@ class NetworkManager {
     static let networkManager = NetworkManager()
     
     let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon/")!
+    var pokemon: Pokemon?
+    
+    func getAllPokemons(completion : @escaping ([PokemonEntry]) -> ()){
+        URLSession.shared.dataTask(with: baseURL) { (data, response, error) in
+            guard let data = data else {
+                return
+            }
+            let pokemonList = try! JSONDecoder().decode(Pokemons.self, from : data)
+            completion(pokemonList.results)
+            
+        }.resume()
+    }
+    
+    func getPokemon(withUrl count: String, completion: @escaping (Pokemon) -> ()) {
+        let requestURL = baseURL.appendingPathComponent(count)
+        let request = URLRequest(url: requestURL)
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            guard let data = data else {
+                return
+            }
+            do{
+                let pokemon = try JSONDecoder().decode(Pokemon.self, from: data)
+                self.pokemon = pokemon
+                completion(pokemon)
+            }catch{
+                print(error.localizedDescription)
+                return
+            }
+        }.resume()
+    }
+
 }
